2396c560bc94043e11269886385d5d6f9f4dab45
Ruben Toro <rubensdt01@gmail.com>
2022-05-12 13:18:23 -0500

    Tab navigation and icons

---
+++ src/navigation/FavoriteNavigation.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import FavoriteScreen from "../screens/Favorite";
+
+const Stack = createStackNavigator();
+
+export default function FavoriteNavigation() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Favorite" component={FavoriteScreen} />
+    </Stack.Navigator>
+  );
+}
--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -1,4 +1,6 @@
+import { Image } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Icon from "react-native-vector-icons/FontAwesome5";
 import FavoriteScreen from "../screens/Favorite";
 import AccountScreen from "../screens/Account";
 import PokedexScreen from "../screens/Pokedex";
@@ -8,9 +10,43 @@ const Tab = createBottomTabNavigator();
 export default function Navigation() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Pokedex" component={PokedexScreen} />
-      <Tab.Screen name="Account" component={AccountScreen} />
-      <Tab.Screen name="Favorite" component={FavoriteScreen} />
+      <Tab.Screen
+        name="Account"
+        component={AccountScreen}
+        options={{
+          tabBarLabel: "Cuenta",
+          tabBarIcon: (color, size) => (
+            <Icon name="user" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Pokedex"
+        component={PokedexScreen}
+        options={{
+          tabBarLabel: "",
+          tabBarIcon: () => renderPokeBall(),
+        }}
+      />
+      <Tab.Screen
+        name="Favorite"
+        component={FavoriteScreen}
+        options={{
+          tabBarLabel: "Favorito",
+          tabBarIcon: (color, size) => (
+            <Icon name="heart" color={color} size={size} />
+          ),
+        }}
+      />
     </Tab.Navigator>
+  );
+}
+
+function renderPokeBall() {
+  return (
+    <Image
+      source={require("../assets/pokeballs.png")}
+      style={{ width: 75, height: 75, top: -15 }}
+    />
   );
 }
