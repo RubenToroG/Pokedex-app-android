13febc60e46c59b5bce611514cb918e1ccf70c2d
Ruben Toro <rubensdt01@gmail.com>
2022-05-12 20:12:40 -0500

    get pokemon info from api, finished

--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -1,9 +1,12 @@
 import { View, Text } from "react-native";
 import React, { useState, useEffect } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { getPokemonsApi } from "../api/pokemon";
+import { getPokemonsApi, getPokemonDetailsByUrlApi } from "../api/pokemon";
 
 export default function Pokedex() {
+  const [pokemons, setPokemons] = useState([]);
+  console.log("pokemons -->", pokemons);
+
   useEffect(() => {
     (async () => {
       await loadPokemons();
@@ -13,7 +16,22 @@ export default function Pokedex() {
   const loadPokemons = async () => {
     try {
       const response = await getPokemonsApi();
-      console.log(response);
+
+      const pokemonsArray = [];
+      for await (const pokemon of response.results) {
+        const pokemonDetails = await getPokemonDetailsByUrlApi(pokemon.url);
+
+        pokemonsArray.push({
+          id: pokemonDetails.id,
+          name: pokemonDetails.name,
+          type: pokemonDetails.types[0].type.name,
+          order: pokemonDetails.order,
+          imagen:
+            pokemonDetails.sprites.other["official-artwork"].front_default,
+        });
+      }
+
+      setPokemons([...pokemons, ...pokemonsArray]);
     } catch (error) {
       console.log(error);
     }
